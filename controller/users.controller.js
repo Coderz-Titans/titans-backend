68a3f029e628b9b4661b0e9bbde9c9a1a2c5d3ae
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-06-27 21:51:43 +0300

    abdallah

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,15 +1,66 @@
 "use strict";
 
 const { userModel } = require("../models/users.model");
+const mongoose = require("mongoose");
 
-const getBooks = (request, response) => {
-  const { email } = request.query;
+// const getUser = (request, response) => {
+//   const { email } = request.query;
 
-  userModel.find({ email: email }, (error, user) => {
-    if (error) {
-      response.send(error);
-    } else {
-      response.json(user);
+//   userModel.find({ email: email }, (error, user) => {
+//     if (error) {
+//       response.send(error);
+//     } else {
+//       response.json(user);
+//     }
+//   });
+// };
+
+//////////////////////////////////////////////////////////////////////////////////
+function newUser(email) {
+  const user = new userModel({
+    email: email,
+    name: email,
+    page: [{
+      viewsOfPage: 0,
+      pageName: email,
+      coverImg: "",
+      profileImg: "",
+      info: "",
+      followersData: [],
+      recipes: [],
+    }],
+
+  }
+  );
+  console.log(user);
+  user.save();
+  return user;
+}
+//////////////////////////////////////////////////////////////////////////////////
+
+function addUser(req, res) {
+  let { email } = req.body;
+
+  userModel.find({ email: email }, (err, userData) => {
+    if (err) {
+      console.log("btata");
+      res.send(err);
     }
-  });
-};
+    else {
+
+      if (userData.length === 0) {
+        console.log("bandora");
+        console.log(email);
+        const user = newUser(email);
+        res.json(user);
+      } else {
+        console.log("khyar");
+        res.json(userData);
+      }
+
+
+    }
+  })
+
+}
+module.exports = addUser;
--- models/recipies.model.js
+++ models/recipies.model.js
@@ -1,14 +1,13 @@
 "use strict";
 
 const mongoose = require("mongoose");
-
 const ratesSchema = require("./rates.model");
 const commentsSchema = require("./comments.model");
 const likesSchema = require("./likes.model");
 
 const recipesSchema = new mongoose.Schema(
   {
-    recipeId: { type: Number, unique: true },
+    // recipeId: { type: Number, unique: true },
     dishImg: { type: String },
     dishTitle: { type: String },
     dishInfo: { type: String },
